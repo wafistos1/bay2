530a0694bdaddfd94b5692c24303cefb2818b6ec
wafi <mameri.wafi@gmail.com>
2022-09-10 12:00:01 +0100

    commit of wafi on  2022-09-10

--- knothome/main.py
+++ knothome/main.py
@@ -1,4 +1,3 @@
-
 import time
 import os
 import requests
@@ -21,19 +20,69 @@ import numpy as np
 #pip install requests pandas openpyxl selenium=3.14 fake_useragent bs4
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
+options = Options()
+ua = UserAgent()
+userAgent = ua.random
+logging.info(userAgent)
+options.add_argument(f'user-agent={userAgent}')
+#opti#     driver = webdriver.Firefox()ons.add_argument("--headless")
+driver = webdriver.Firefox(firefox_options=options)
+
 @dataclass
 class Product:
-    link_url : str = ''
-    name : str = ''
-    price : str = ''
-    description : str = ''
-    base_image : str =  ''
-    add_image :list = field(default_factory=list)
-    cat1 : str = ''
-    cat2 : str = ''
-    cat3 : str = ''
+    link_url: str = ''
+    name: str = ''
+    price: str = ''
+    special_price: str = ''
+    description: str = ''
+    raw_materials: str = ''
+    images_size: str = ''
+    base_image: str =  ''
+    add_images: str = ''
+    cat1: str = ''
+    cat2: str = ''
+    cat3: str = ''
 
-urls = pd.read_excel('path excel url file')
+
+def extract_data(driver, url1):
+    product.cat1 = url1['cat1']
+    product.cat2 = url1['cat2']
+    product.cat3 = url1['cat3']
+    url = url1['url']
+    logging.info('URL: %s', url)
+    driver.get(url)
+    time.sleep(2)
+    product = Product()
+    body = WebDriverWait(driver,20).until(EC.visibility_of_element_located((By.TAG_NAME,'body')))
+    try:
+        body.find_element(By.ID, 'wg-ar').click()
+    except:
+        pass
+    r = driver.page_source
+    soup = BeautifulSoup(r, "html.parser")
+    product.link_url = url
+    product.name = soup.find('div', {'class': 'product__title'}).find('span').text.strip()
+    product.price = soup.find('span', {'class': 'money'}).text.strip()
+    product.description = soup.find('div', {'class': 'product__text'}).text.strip()
+    try:
+        product.raw_materials = ', '.join([rw.text for rw in soup.find('button', {'class': 'product__tab-button'}, text=re.compile('المواد')).parent.find_all('li')])
+    except:
+        pass
+    try:
+        product.images_size = soup.find('button', {'class': 'product__tab-button'}, text=re.compile('حجم المنتج')).parent.find('img')['src']
+    except:
+        pass
+    images = soup.find_all('div', {'class': 'product__gallery-item'})
+    list_images = ['https:' + img.find('img', {'src': re.compile(r'^//cdn.shopify.com/s/files')})['src'].replace('110x110@', '').split('?')[0] for img in images]
+    list_images  = list(dict.fromkeys(list_images))
+    list_images
+    product.base_image = list_images[0]
+    product.add_images = ','.join(list_images[1:])
+    return product.__dict__
+
+
+
+urls = pd.read_csv('knothhome_url_mupdate.csv')
 list_urls = []
 for index, row in urls.iterrows():
     list_urls.append(
@@ -45,22 +94,15 @@ for index, row in urls.iterrows():
         }
     )
 
-def scrape_data(url1):
-    cat1 = url1['cat1']
-    cat2 = url1['cat2']
-    cat3 = url1['cat3']
-    url = url1['url']
-    logging.info('URL: %s', url)
-    
-    return {
-        
-    }
+def scrape_data(url1, driver):
+    data = extract_data(driver, url1)
+    return data
 
-df = pd.read_excel('path excel url file')
-for i, url in enumerate(urls):
+df = pd.read_csv('knothhome_product_model.csv')
+for i, url in enumerate(list_urls):
     logging.info('Count: %s', i)
     data = scrape_data(url)
     df1 = pd.DataFrame([data])
     df = pd.concat([df, df1], ignore_index=True)
-    df.to_excel()
+    df.to_excel('knothhome_product_update.xlsx')
 
